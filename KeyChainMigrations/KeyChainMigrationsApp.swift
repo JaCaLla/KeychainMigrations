2e50973174894b3122d2581d5f287747f9480817
Javier Calatrava <javi.calatrava@gmail.com>
2025-06-12 20:32:44 +0200

    Create blank iOSProject

---
+++ KeyChainMigrations/KeyChainMigrationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyChainMigrationsApp.swift
+//  KeyChainMigrations
+//
+//  Created by JAVIER CALATRAVA LLAVERIA on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct KeyChainMigrationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KeyChainMigrationsTests/KeyChainMigrationsTests.swift
@@ -0,0 +1,17 @@
+//
+//  KeyChainMigrationsTests.swift
+//  KeyChainMigrationsTests
+//
+//  Created by JAVIER CALATRAVA LLAVERIA on 12/6/25.
+//
+
+import Testing
+@testable import KeyChainMigrations
+
+struct KeyChainMigrationsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
